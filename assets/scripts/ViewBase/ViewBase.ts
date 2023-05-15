a1bccd1629c7599285d4d10c2ea54511e3c10299
NoRainC <wuxinrufeng@qq.com>
2023-05-15 17:58:03 +0800

    修改

---
+++ assets/scripts/ViewBase/ViewBase.ts
@@ -0,0 +1,28 @@
+import { Component } from "cc";
+
+/*
+ * @Author: NoRain 
+ * @Date: 2023-05-15 11:27:34 
+ * @Last Modified by: NoRain
+ * @Last Modified time: 2023-05-15 11:55:26
+ */
+export default class ViewBase extends Component {
+
+    onOpen() {
+
+    }
+    addEvent() {
+
+    }
+    removeEvent() {
+
+    }
+    update() {
+        // let time = 
+    }
+
+    onClose() {
+
+    }
+
+}
